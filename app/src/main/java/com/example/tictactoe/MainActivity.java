bfd60901d11f9e7f1d3e5a3c660770c5df71916a
Goy288 <44177200+Goy288@users.noreply.github.com>
2020-05-06 15:10:14 -0700

    Implemented button functionality, and the status text now displays whose turn it is.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.tictactoe;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.ActionBar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isPlayerXTurn;
+    //private boolean isResetState;
+
+    private TextView statusText;
+    private Button[] buttonBoard;
+    private Button newGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,86 @@ public class MainActivity extends AppCompatActivity {
         */
 
         setContentView(R.layout.activity_main);
+
+        statusText = findViewById(R.id.txt_status);
+        buttonBoard = new Button[] { findViewById(R.id.btn_slot_topLeft),
+                                     findViewById(R.id.btn_slot_topCenter),
+                                     findViewById(R.id.btn_slot_topRight),
+                                     findViewById(R.id.btn_slot_middleLeft),
+                                     findViewById(R.id.btn_slot_middleCenter),
+                                     findViewById(R.id.btn_slot_middleRight),
+                                     findViewById(R.id.btn_slot_bottomLeft),
+                                     findViewById(R.id.btn_slot_bottomCenter),
+                                     findViewById(R.id.btn_slot_bottomRight) };
+        newGameButton = findViewById(R.id.btn_newGame);
+
+        newGameButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v){
+                        newGame();
+                    }
+                }
+        );
+        for ( final Button button : buttonBoard ) {
+            button.setOnClickListener(
+                    new Button.OnClickListener() {
+                        public void onClick(View v){
+                            changeButtonSymbol(button);
+                        }
+                    }
+            );
+        }
     }
+
+    private void changeButtonSymbol(Button selectedBtn){
+        if( selectedBtn.getText() == "" ) {
+            selectedBtn.setText( isPlayerXTurn ? "X" : "O" );
+            isPlayerXTurn = !isPlayerXTurn;
+            changeStatusText();
+        }
+    }
+
+    private void changeStatusText(){
+        String StatusMessage;
+        StatusMessage = isPlayerXTurn ? "Player X's Turn" : "Player O's Turn";
+        statusText.setText( StatusMessage );
+    }
+
+    private void newGame(){
+        isPlayerXTurn = false;
+        for ( Button button : buttonBoard ) {
+            button.setText("");
+        }
+        changeStatusText();
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putBoolean( "isPlayerXTurn", isPlayerXTurn );
+
+        CharSequence[] buttonBoardValues = new String[ buttonBoard.length ];
+        for( int i = 0; i < buttonBoard.length; i++ ) {
+            buttonBoardValues[i] = buttonBoard[i].getText();
+        }
+        outState.putCharSequenceArray( "buttonBoardValues", buttonBoardValues );
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        isPlayerXTurn = savedInstanceState.getBoolean( "isPlayerXTurn" );
+        changeStatusText();
+
+        CharSequence[] buttonBoardValues =
+                savedInstanceState.getCharSequenceArray( "buttonBoardValues" );
+
+        for( int i = 0; i < buttonBoard.length; i++ ) {
+            buttonBoard[i].setText(buttonBoardValues[i]);
+        }
+    }
+
 }
